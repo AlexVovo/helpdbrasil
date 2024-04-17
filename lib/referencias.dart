70198d9066bdfe360e306bb8ca5a39b7df281bc2
Alexsandro Ávila <57649224+AlexVovo@users.noreply.github.com>
2024-04-16 23:06:35 -0300

    up

--- lib/referencias.dart
+++ lib/referencias.dart
@@ -8,7 +8,15 @@ class ReferenciasScreen extends StatelessWidget {
         title: Text('Referências'),
       ),
       body: Center(
-        child: Text('Página de Referências'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Página de Referências'),
+            SizedBox(height: 20), // Espaçamento entre os textos
+            Text(
+                '\nSOFTWARES UTILIZADOS PARA DESENVOLVIMENTO\n\n* Flutter(Dart): Flutter é uma estrutura de código aberto do Google para criar aplicativos multiplataforma bonitos e compilados nativamente a partir de uma única base de código.\n\n* Google Material Design:  um sistema de design criado pelo Google para ajudar as equipes a criar experiências digitais de alta qualidade para Android, iOS, Flutter e Web.\n\n* Microsoft Visual Studio Code\n\n* Ícones: flaticon.com\n\nOUTRAS BIBLIOGRAFIAS\n\n* Hamilton scale (HAM-D 17 items)\n\n'), // Texto adicional
+          ],
+        ),
       ),
     );
   }
