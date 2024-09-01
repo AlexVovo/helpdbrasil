aae2f116d8876152ec668ad3e13ea60611e78ea7
Alexsandro Ávila <57649224+AlexVovo@users.noreply.github.com>
2024-09-01 12:26:53 -0300

    update

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/alex/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/alex/Documentos/GitHub/helpdepressao"
+export "FLUTTER_APPLICATION_PATH=/home/alex/Documentos/GitHub/helpdbrasil"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
